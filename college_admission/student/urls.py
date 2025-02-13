c52ca77004ae8d4f5f4ba492840f8171d3741e7e
sekhonmanjit001 <sekhonmanjit38@gmail.com>
2025-02-13 22:51:33 +0530

    register page

--- college_admission/student/urls.py
+++ college_admission/student/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [ 
     path('', views.view_name),
+    path('register', views.signUp_register),
 ]
+
--- college_admission/student/views.py
+++ college_admission/student/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def view_name (request):
     return render(request,"landingPage.html")
 # Create your views here.
+def signUp_register(request):
+    return render(request,"Registration_student.html")
